f82467e434fe5abf601dfc7cfc6ed94545ed586d
PThorpe92 <preston@unlockedlabs.org>
2023-09-04 15:31:47 -0400

    work: continuing to build backend

--- src/curl.rs
+++ src/curl.rs
@@ -1,4 +1,6 @@
 use crate::Request;
+use reqwest::Response;
+use std::process::Command;
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Curl {
@@ -7,13 +9,151 @@ pub struct Curl {
     pub req: Request,               // The final request we will make
     pub resp: Option<&'static str>, // The response we get back from the command if not sent to file
 }
+
 impl Curl {
-    pub fn default() -> Self {
+    pub fn default(url: &str) -> Self {
         Self {
             cmd: "curl",
             opts: Vec::new(),
-            req: Request::default(),
+            req: Request::default(url),
             resp: None,
+        }
+    }
+    pub fn add_flag(&mut self, flag: CurlFlag, value: Option<String>) {
+        match flag {
+            CurlFlag::Verbose(_) => self.opts.push(Flag {
+                flag: CurlFlag::Verbose(String::from("-v")),
+                value: None,
+            }),
+            CurlFlag::AnyAuth(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::AnyAuth),
+                value: None,
+            }),
+            CurlFlag::Ntlm(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Ntlm),
+                value: None,
+            }),
+            CurlFlag::Output(_) => self.opts.push(Flag {
+                flag: CurlFlag::Output(String::from("-o")),
+                value: Some(String::from(value.expect("Output file not provided"))),
+            }),
+            CurlFlag::Trace(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Trace),
+                value: Some(String::from(
+                    value.expect("File to write trace info to not provided"),
+                )),
+            }),
+            CurlFlag::DataUrlEncode(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::DataUrlEncode),
+                value: Some(String::from(
+                    value.expect("Data to url encode not provided"),
+                )),
+            }),
+            CurlFlag::DumpHeaders(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::DumpHeaders),
+                value: Some(String::from(
+                    value.expect("File to dump headers to not provided"),
+                )),
+            }),
+            CurlFlag::Referrer(_) => self.opts.push(Flag {
+                flag: CurlFlag::Referrer(String::from("-e")),
+                value: Some(String::from(value.expect("Referrer not provided"))),
+            }),
+            CurlFlag::Insecure(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Insecure),
+                value: None,
+            }),
+            CurlFlag::User(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::User),
+                value: Some(String::from(value.expect("username:password not provided"))),
+            }),
+            CurlFlag::Bearer(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Bearer),
+                value: Some(String::from(value.expect("Bearer token not provided"))),
+            }),
+            CurlFlag::Digest(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Digest),
+                value: Some(String::from(
+                    value.expect("Initial digest request header not provided"),
+                )),
+            }),
+            CurlFlag::Basic(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Basic),
+                value: Some(String::from(value.expect("username:password not provided"))),
+            }),
+            CurlFlag::Socks5(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Socks5),
+                value: Some(String::from(value.expect("Socks5 info not provided"))),
+            }),
+            CurlFlag::CaCert(_) => self.opts.push(Flag {
+                flag: CurlFlag::CaCert(String::from("--cacert")),
+                value: Some(String::from(value.expect("Certificate not provided"))),
+            }),
+            CurlFlag::CaNative(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::CaNative),
+                value: None,
+            }),
+            CurlFlag::File(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::File),
+                value: Some(String::from(value.expect("File not provided"))),
+            }),
+            CurlFlag::FtpAccount(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::FtpAccount),
+                value: Some(String::from(value.expect("FTP account not provided"))),
+            }),
+            CurlFlag::FtpSsl(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::FtpSsl),
+                value: None,
+            }),
+            CurlFlag::CaPath(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::CaPath),
+                value: Some(String::from(
+                    value.expect("Directory for CaPath not provided"),
+                )),
+            }),
+            CurlFlag::ProxyTunnel(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::ProxyTunnel),
+                value: None,
+            }),
+            CurlFlag::PreventDefaultConfig(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::PreventDefaultConfig),
+                value: None,
+            }),
+            CurlFlag::UnixSocket(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::UnixSocket),
+                value: Some(String::from(value.expect("Socket info not provided"))),
+            }),
+            CurlFlag::UploadFile(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::UploadFile),
+                value: Some(String::from(value.expect("Upload file value not provided"))),
+            }),
+            CurlFlag::Proxy(_) => self.opts.push(Flag {
+                flag: CurlFlag::new(None, CurlFlagType::Proxy),
+                value: Some(String::from(value.expect("Proxy value not provided"))),
+            }),
+        }
+        self.opts.push(Flag { flag, value });
+    }
+    pub fn execute_command(&mut self) {
+        let mut output = Command::new("curl");
+        // Add our flags to the command
+        self.opts.into_iter().map(|f| {
+            // if our flag has an argument, add it to the command
+            output.arg(f.flag.get_value().as_str());
+            if let Some(argument) = f.value {
+                output.arg(argument);
+            }
+        });
+        output.arg(self.req.url.as_str());
+
+        // As long as the choices were presented to the user in the proper order,
+        // adding each command line flag with it's value/arg at a time should
+        // result in a valid command
+
+        // Check if the command was successful
+        if output.status().unwrap().success() {
+            // Convert the stdout bytes to a string
+            let response = String::from_utf8_lossy(&output.stdout);
         }
     }
 }
@@ -23,59 +163,89 @@ impl Curl {
 #[derive(Debug, Clone, PartialEq)]
 pub struct Flag {
     pub flag: CurlFlag,
-    pub value: String,
+    pub value: Option<String>,
 }
 
-#[derive(Debug, Clone, PartialEq)]
-pub enum CurlFlag {
-    Verbose(String),
-    Output(String),
-    User(String),
-    Bearer(String),
-    Digest(String),
-    Basic(String),
-    AnyAuth(String),
-    UnixSocket(String),
-    UploadFile(String),
-    Ntlm(String),
-    Proxy(String),
-    ProxyTunnel(String),
-    Socks5(String),
-    Dict(String),
-    File(String),
-    FtpAccount(String),
-    FtpSsl(String),
-    Trace(String),
-    DataUrlEncode(String),
-    DumpHeaders(String),
-    Referrer(String),
-    PreventDefaultConfig(String),
+#[macro_export]
+macro_rules! define_curl_flags {
+    (
+        $( $variant:ident($value:expr), )*
+    ) => {
+        #[derive(Debug, Clone, PartialEq)]
+        pub enum CurlFlag {
+            $( $variant(String), )*
+        }
+
+        impl CurlFlag {
+            pub fn new(value: Option<String>, flag_type: CurlFlagType) -> Self {
+                match flag_type {
+                    $( CurlFlagType::$variant => CurlFlag::$variant(value.unwrap_or($value.to_string())), )*
+                }
+            }
+        }
+
+        #[derive(Debug, Clone, PartialEq)]
+        enum CurlFlagType {
+            $( $variant, )*
+        }
+    };
 }
 impl CurlFlag {
-    pub fn new(&self) -> Self {
-        match self {
-            CurlFlag::AnyAuth(_) => CurlFlag::AnyAuth(String::from("--any-auth")),
-            CurlFlag::Verbose(_) => CurlFlag::Verbose(String::from("-v")),
-            CurlFlag::Output(_) => CurlFlag::Output(String::from("-o")),
-            CurlFlag::User(_) => CurlFlag::User(String::from("-u")),
-            CurlFlag::Bearer(_) => CurlFlag::Bearer(String::from("-H")),
-            CurlFlag::Digest(_) => CurlFlag::Digest(String::from("--digest")),
-            CurlFlag::Basic(_) => CurlFlag::Basic(String::from("-H")),
-            CurlFlag::UnixSocket(_) => CurlFlag::UnixSocket(String::from("--unix-socket")),
-            CurlFlag::UploadFile(_) => CurlFlag::UploadFile(String::from("--upload-file")),
-            CurlFlag::Proxy(_) => CurlFlag::Proxy(String::from("-x")),
-            CurlFlag::Dict(_) => CurlFlag::Dict(String::from("-")),
-            CurlFlag::File(_) => CurlFlag::File(String::from("-F")),
-            CurlFlag::Socks5(_) => CurlFlag::Socks5(String::from("--socks5")),
-            CurlFlag::FtpAccount(_) => CurlFlag::FtpAccount(String::from("--ftp-account")),
-            CurlFlag::Ntlm(_) => CurlFlag::Ntlm(String::from("--ntlm")),
-            CurlFlag::ProxyTunnel(_) => CurlFlag::ProxyTunnel(String::from("--proxy-tunnel")),
-            CurlFlag::Trace(_) => CurlFlag::Trace(String::from("--trace")),
-            CurlFlag::DumpHeaders(_) => CurlFlag::DumpHeaders(String::from("--dump-headers")),
-            CurlFlag::DataUrlEncode(_) => CurlFlag::DataUrlEncode(String::from("--data-urlencode")),
-            CurlFlag::Referrer(_) => CurlFlag::Referrer(String::from("-e")),
-            CurlFlag::PreventDefaultConfig(_) => CurlFlag::PreventDefaultConfig(String::from("-q")),
-            CurlFlag::FtpSsl(_) => CurlFlag::FtpSsl(String::from("--ftp-ssl")),
+    pub fn get_value(&self) -> String {
+        match *self {
+            CurlFlag::Verbose(val) => val,
+            CurlFlag::Output(val) => val,
+            CurlFlag::User(val) => val,
+            CurlFlag::Bearer(val) => val,
+            CurlFlag::Digest(val) => val,
+            CurlFlag::Basic(val) => val,
+            CurlFlag::AnyAuth(val) => val,
+            CurlFlag::UnixSocket(val) => val,
+            CurlFlag::UploadFile(val) => val,
+            CurlFlag::Ntlm(val) => val,
+            CurlFlag::Proxy(val) => val,
+            CurlFlag::ProxyTunnel(val) => val,
+            CurlFlag::Socks5(val) => val,
+            CurlFlag::File(val) => val,
+            CurlFlag::FtpAccount(val) => val,
+            CurlFlag::FtpSsl(val) => val,
+            CurlFlag::Trace(val) => val,
+            CurlFlag::DataUrlEncode(val) => val,
+            CurlFlag::DumpHeaders(val) => val,
+            CurlFlag::Referrer(val) => val,
+            CurlFlag::Insecure(val) => val,
+            CurlFlag::PreventDefaultConfig(val) => val,
+            CurlFlag::CaCert(val) => val,
+            CurlFlag::CaNative(val) => val,
+            CurlFlag::CaPath(val) => val,
         }
     }
+}
+// Define the CurlFlag enum using the macro.
+define_curl_flags! {
+    Verbose("-v"),
+    Output("-o"),
+    User("-u"),
+    Bearer("-H"),
+    Digest("--digest"),
+    Basic("-H"),
+    AnyAuth("--any-auth"),
+    UnixSocket("--unix-socket"),
+    UploadFile("--upload-file"),
+    Ntlm("--ntlm"),
+    Proxy("-x"),
+    ProxyTunnel("--proxy-tunnel"),
+    Socks5("--socks5"),
+    File("-F"),
+    FtpAccount("--ftp-account"),
+    FtpSsl("--ftp-ssl"),
+    Trace("--trace"),
+    DataUrlEncode("--data-urlencode"),
+    DumpHeaders("--dump-headers"),
+    Referrer("-e"),
+    Insecure("--insecure"),
+    PreventDefaultConfig("-q"),
+    CaCert("--cacert"),
+    CaNative("--ca-native"),
+    CaPath("--capath"),
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use reqwest::{Client, Method};
 use std::fs::File;
 use std::io::{BufWriter, Write};
+
 /// Application.
 pub mod app;
 
@@ -16,44 +17,91 @@ pub mod tui;
 /// Event handler.
 pub mod handler;
 
+// Curl command builder
 pub mod curl;
 
+// Wget command builder
 pub mod wget;
 
-pub static GET: &str = "get";
-pub static POST: &str = "post";
-pub static PUT: &str = "put";
-pub static DELETE: &str = "delete";
-pub static PATCH: &str = "patch";
-pub static HEAD: &str = "head";
-pub static OPTIONS: &str = "options";
+pub static GET: &str = "GET";
+pub static POST: &str = "POST";
+pub static PUT: &str = "PUT";
+pub static DELETE: &str = "DELETE";
+pub static PATCH: &str = "PATCH";
+pub static HEAD: &str = "HEAD";
+pub static OPTIONS: &str = "OPTIONS";
 
 #[derive(Debug, Clone, PartialEq)]
 pub struct Request {
     pub cmd: CmdType,                                       // curl, wget, custom
     pub req_type: &'static str,                             // get, post, put, delete
-    pub url: &'static str,                                  // the url to hit
+    pub url: &'static str,                                  // the url to send the request to
     pub headers: Option<Vec<(&'static str, &'static str)>>, // header collection in (key, value) pairs
     pub body: Option<&'static str>,                         // the body to send
     pub timeout: u32,                                       // how long to wait for a response
-    pub auth: &'static str,                                 // basic, bearer, digest, custom
+    pub auth: Auth,                                         // basic, bearer, digest, custom
     pub output: Option<&'static str>,                       // where to write the output
 }
 
 impl Request {
-    pub fn default() -> Self {
+    // default method is GET
+    pub fn default(url: &str) -> Self {
         Request {
             cmd: CmdType::Curl,
-            req_type: "get",
-            url: "https://localhost",
+            req_type: GET,
+            url,
             headers: Some(vec![
                 ("User-Agent", "Curl"),
                 ("Content-Type", "application/json"),
             ]),
             body: None,
             timeout: 30,
-            auth: "basic",
+            auth: Auth::AnyAuth,
             output: None,
+        }
+    }
+    pub fn new(
+        cmd: CmdType,
+        req_type: &'static str,
+        url: &'static str,
+        headers: Option<Vec<(&'static str, &'static str)>>,
+        body: Option<&'static str>,
+        timeout: u32,
+        auth: Auth,
+        output: Option<&'static str>,
+    ) -> Self {
+        Request {
+            cmd,
+            req_type,
+            url,
+            headers,
+            body,
+            timeout,
+            auth,
+            output,
+        }
+    }
+    pub fn add_url(&mut self, url: &'static str) {
+        self.url = url;
+    }
+    pub fn add_headers(&mut self, headers: Vec<(&'static str, &'static str)>) {
+        self.headers = Some(headers);
+    }
+    pub fn add_auth(&mut self, auth: Auth) {
+        match auth {
+            AnyAuth => self.auth = Auth::AnyAuth,
+            // val in this case should be "username:password" string
+            Auth::Basic(username_pwd) => {
+                self.auth = Auth::Basic(username_pwd);
+                self.add_headers(vec![("Authorization", "Basic")]);
+                self.add_headers(vec![(username_pwd.as_str(), "")]);
+            }
+            Auth::Bearer(token) => {
+                self.auth = Auth::Bearer(token);
+                self.add_headers(vec![("Bearer: token", token.as_str())])
+            }
+            Auth::Digest(val) => {} // skip this for now...
+            Auth::Custom(val) => self.auth = Auth::Custom(val),
         }
     }
     pub async fn send_request(&self) -> Result<(), reqwest::Error> {
@@ -81,7 +129,7 @@ impl Request {
 
         // Set authentication
         match self.auth {
-            "basic" => {
+            Auth::Basic(val) => {
                 // Implement basic authentication
             }
             "bearer" => {
@@ -120,11 +168,127 @@ impl Request {
     }
 }
 #[derive(Debug, Clone, PartialEq)]
-pub enum AuthType {
-    Basic,
-    Bearer,
-    Digest,
-    Custom,
+pub enum Auth {
+    AnyAuth,
+    Basic(String),
+    Bearer(String),
+    Digest {
+        username: String,
+        password: String,
+        realm: String,
+        nonce: String,
+        qop: String,
+        nc: String,
+        cnonce: String,
+        uri: String,
+        method: String,
+    },
+    Custom(String),
+    Ntlm(String),
+    Spnego(String),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DigestAuth {
+    realm: String,
+    nonce: String,
+    qop: String,
+    nc: String,
+    cnonce: String,
+    uri: String,
+    username: String,
+    password: String,
+    method: String,
+}
+impl DigestAuth {
+    pub fn new(realm: &str, nonce: &str, qop: &str, nc: &str, cnonce: &str, uri: &str) -> Self {
+        DigestAuth {
+            realm: realm.to_string(),
+            nonce: nonce.to_string(),
+            qop: qop.to_string(),
+            nc: nc.to_string(),
+            cnonce: cnonce.to_string(),
+            uri: uri.to_string(),
+            username: String::new(),
+            password: String::new(),
+            method: String::new(),
+        }
+    }
+
+    // When we initiate a request that requires digest authentication from an HTTP server,
+    // the response header will look like this:
+    //
+    // HTTP/1.1 401 Unauthorized
+    // WWW-Authenticate: Digest realm="Example Realm", qop="auth", nonce="UniqueNonce", opaque="OpaqueValue"
+    //
+    // looks lke we need to calculate our next request's auth (represented by this DigestAuth struct) by parsing
+    // the response headers using Sha256
+    // so it seems an auth.rs file is in order... or a response.rs file and we can handle all the
+    // response parsing there.
+
+    pub fn from_headers(headers: Vec<(&str, &str)>) -> Self {
+        let mut realm = String::new();
+        let mut nonce = String::new();
+        let mut qop = String::new();
+        let mut nc = String::new();
+        let mut cnonce = String::new();
+        let mut uri = String::new();
+        for (key, value) in headers {
+            match key {
+                "realm" => realm = value.to_string(),
+                "nonce" => nonce = value.to_string(),
+                "qop" => qop = value.to_string(),
+                "nc" => nc = value.to_string(),
+                "cnonce" => cnonce = value.to_string(),
+                "uri" => uri = value.to_string(),
+                _ => {}
+            }
+        }
+        DigestAuth {
+            realm,
+            nonce,
+            qop,
+            nc,
+            cnonce,
+            uri,
+            username: String::new(),
+            password: String::new(),
+            method: String::new(),
+        }
+    }
+}
+
+impl Auth {
+    pub fn new(
+        auth: &str,
+        info: &str,
+        pos: Option<&str>,
+        digest: Option<DigestAuth>,
+    ) -> Result<Auth, String> {
+        match auth {
+            "basic" => Ok(Auth::Basic(info.to_string())),
+            "bearer" => Ok(Auth::Bearer(info.to_string())),
+            "digest" => match digest {
+                Some(digest) => Ok(Auth::Digest {
+                    username: digest.username,
+                    password: digest.password,
+                    realm: digest.realm,
+                    nonce: digest.nonce,
+                    qop: digest.qop,
+                    nc: digest.nc,
+                    cnonce: digest.cnonce,
+                    uri: digest.uri,
+                    method: digest.method,
+                }),
+                None => Err("Digest authentication requires a username and password".to_string()),
+            },
+            "custom" => Ok(Auth::Custom(info.to_string())),
+            "spnego" => Ok(Auth::Spnego(info.to_string())),
+            "ntlm" => Ok(Auth::Ntlm(info.to_string())),
+
+            _ => Ok(Auth::Basic(info.to_string())),
+        }
+    }
 }
 
 #[derive(Debug, Clone, PartialEq)]
